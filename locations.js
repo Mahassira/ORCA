457f4fe9f9c7d8c2cda6b112f8423cb633c34e02
Mahassira <Mahassira@outlook.com>
2026-09-19 15:11:28 +0300

    Replace tile layer with OpenStreetMap tiles

--- locations.js
+++ locations.js
@@ -137,9 +137,10 @@
       tap: true
     });
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-      maxZoom: 19
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      maxZoom: 19,
+      className: 'loc-map-tiles'
     }).addTo(map);
 
     var bounds = [];
